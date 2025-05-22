8f86d7d217144e4dbeae05c29e7e41fdfcc27c39
SATHVIK S.R <131685174+sathviksr2001@users.noreply.github.com>
2025-05-22 11:07:25 +0530

    Create eventController.js

---
+++ controllers/eventController.js
@@ -0,0 +1,20 @@
+const Event = require('../models/Event');
+
+exports.createEvent = async (req, res) => {
+  try {
+    const event = new Event(req.body);
+    await event.save();
+    res.status(201).json(event);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getEvents = async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
